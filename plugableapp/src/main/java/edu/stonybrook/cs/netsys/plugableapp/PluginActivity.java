8563180e2b0711e72517c2cdbc29b71a6c73370c
Arron Cao <qqcao.cs@gmail.com>
2016-11-08 02:02:58 -0500

    add: card type xml and activity code
    todos: 1. writing wear proxy code and integrate with current manually written plugable app;2. extract fields/data in manual plugable app and define communication protocol for wear proxy then autogen plugable app; 3. integrate metaprogram with view tree extraction together and test

--- plugableapp/src/main/java/edu/stonybrook/cs/netsys/plugableapp/PluginActivity.java
+++ plugableapp/src/main/java/edu/stonybrook/cs/netsys/plugableapp/PluginActivity.java
@@ -1,93 +1,36 @@
 package edu.stonybrook.cs.netsys.plugableapp;
 
-import static edu.stonybrook.cs.netsys.uiwearlib.Constant.WEAR_PLUGIN_KEY;
-
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.util.Pair;
-import android.support.wearable.view.WearableListView;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-
-public class PluginActivity extends Activity implements WearableListView.ClickListener {
+public class PluginActivity extends Activity{
     ResReceiver resReceiver;
-    ListAdapter adapter;
-
-    private ArrayList<Pair<Drawable, String[]>> listItems = new ArrayList<>();
-    private static ArrayList<Integer> ids = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recentlyplayed_main);
-        Intent intent = getIntent();
-        if (intent != null && intent.getStringExtra(WEAR_PLUGIN_KEY) != null) {
-            String words = "say: " + intent.getStringExtra(WEAR_PLUGIN_KEY);
-            Toast.makeText(this, words, Toast.LENGTH_SHORT).show();
-        }
+        setContentView(R.layout.activity_container);
 
-        WearableListView mListView = (WearableListView) findViewById(R.id.content_list);
-        init();
-        adapter = new ListAdapter(this);
-        mListView.setAdapter(adapter);
-        mListView.setClickListener(PluginActivity.this);
-        mListView.setKeepScreenOn(true);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // we need to auto generate and replace layoutId based on wear proxy transferred id
+        int layoutID = R.layout.card;
+
+        UIWearFragment card = UIWearFragment.create(layoutID);
+        fragmentTransaction.add(R.id.frame_layout_container, card);
+        fragmentTransaction.commit();
 
     }
 
-//    public void goHost(View view) {
-//        Logger.i("goHost");
-//        if (isActionAvailable(this, WEAR_HOST_ACTION_MAIN)) {
-//            Intent intent = new Intent(WEAR_HOST_ACTION_MAIN);
-//            intent.putExtra(WEAR_HOST_KEY, "Hello, host!");
-//            startActivity(intent);
-//        } else {
-//            Toast.makeText(this, "Failed to go host", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    public void setResult(View view) {
-//        Logger.i("set result");
-//        Intent resultIntent = new Intent();
-//        resultIntent.putExtra(WEAR_HOST_KEY, "result from plugin");
-//        setResult(Activity.RESULT_OK, resultIntent);
-//        finish();
-//    }
-
-    public void init() {
-        listItems.clear();
-        Drawable d = getDrawable(R.drawable.hangouts_default_avatar);
-        String[] defaultStr = {"TestDefault", "LastMsg"};
-        Pair<Drawable, String[]> d1 = new Pair<>(d, defaultStr);
-        listItems.add(d1);
-    }
-
-    @Override
-    public void onClick(WearableListView.ViewHolder viewHolder) {
-        int pos = viewHolder.getLayoutPosition();
-        if (ids != null && ids.size() > 0) {
-            // TODO: 10/31/16 Monday talk to wear proxy via broadcast is better
-//            Intent intent = new Intent(getApplicationContext(),
-//                    ViewTreeService.class);
-//            intent.putExtra(Constants.HANGOUTS_PKG + "hangoutId", ids.get(pos));
-//            startService(intent);
-        }
-    }
-
-    @Override
-    public void onTopEmptyRegionClick() {
-
-    }
+    // TODO: 10/31/16 Monday talk to wear proxy via broadcast is bette
+    // click listener send id to wear proxy via LocalBroadcastManager
 
     @Override
     protected void onStart() {
@@ -96,8 +39,6 @@ public class PluginActivity extends Activity implements WearableListView.ClickLi
         resReceiver = new ResReceiver();
         IntentFilter intentFilter = new IntentFilter();
         // TODO: 10/31/16 Monday register intent filter for talking to wear proxy
-//        intentFilter.addAction(Constants.HANGOUTS_PKG);
-//        intentFilter.addAction(Constants.WINDOW_CHANGE);
         registerReceiver(resReceiver, intentFilter);
         //Start our own service
 //        Intent intent = new Intent(AndroidServiceTestActivity.this,
@@ -112,78 +53,16 @@ public class PluginActivity extends Activity implements WearableListView.ClickLi
         super.onStop();
     }
 
-    public class ListAdapter extends WearableListView.Adapter {
-        private final LayoutInflater mInflater;
-
-        private ListAdapter(Context context) {
-            mInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new WearableListView.ViewHolder(
-                    mInflater.inflate(R.layout.recently_played_list_item, parent, false));
-        }
-
-        @Override
-        public void onBindViewHolder(WearableListView.ViewHolder holder, int i) {
-
-            Pair<Drawable, String[]> pair = listItems.get(i);
-
-            Drawable drawable = pair.first;
-            ImageView imgView = (ImageView) holder.itemView.findViewById(R.id.thumbnail);
-            imgView.setBackground(drawable);
-
-            TextView songTextView = (TextView) holder.itemView.findViewById(R.id.text1);
-            songTextView.setText(pair.second[0]);
-            TextView albumTextView = (TextView) holder.itemView.findViewById(R.id.text2);
-            albumTextView.setText(pair.second[1]);
-
-            holder.itemView.setTag(i);
-        }
-
-        @Override
-        public int getItemCount() {
-            return listItems.size();
-        }
-    }
 
     private class ResReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            // TODO: 10/31/16 Monday refresh data
-//            if (Constants.WINDOW_CHANGE.equals(intent.getAction())) {
-//                init();
-//                adapter.notifyDataSetChanged();
-//            }
-
-            // TODO: 10/31/16 Monday receive data from wear proxy
-//            if (Constants.HANGOUTS_PKG.equals(intent.getAction())) {
-
-            listItems.clear();
-//                ArrayList<byte[]> imgList = (ArrayList<byte[]>) intent.
-//                        getSerializableExtra(Constants.HANGOUTS_PKG + "imgList");
-//
-//                ArrayList<String> nameList = (ArrayList<String>) intent.
-//                        getSerializableExtra(Constants.HANGOUTS_PKG + "nameList");
-//                ArrayList<String> msgList = (ArrayList<String>) intent.
-//                        getSerializableExtra(Constants.HANGOUTS_PKG + "msgList");
+            // TODO: 10/31/16 Monday receive card data(four info)  from wear proxy
 
             // TODO: 10/31/16 Monday parse data(bitmap etc.) from wear proxy in background thread
-//                for (int i = 0; i < imgList.size(); i++) {
-//                    byte[] imgBytes = imgList.get(i);
-//                    Drawable drawable = new BitmapDrawable(getResources(),
-//                            BitmapFactory.decodeByteArray(imgBytes, 0, imgBytes.length));
-//
-//                    String[] text = {nameList.get(i),msgList.get(i)};
-//                    Pair<Drawable, String[]> pair = new Pair<>(drawable, text);
-//                    listItems.add(pair);
-//                }
 
             // TODO: 10/31/16 Monday record listener id for control back
-//                ids = intent.getIntegerArrayListExtra(Constants.HANGOUTS_PKG + "hangoutId");
-            adapter.notifyDataSetChanged();
         }
     }
 
---
+++ plugableapp/src/main/java/edu/stonybrook/cs/netsys/plugableapp/UIWearFragment.java
@@ -0,0 +1,41 @@
+package edu.stonybrook.cs.netsys.plugableapp;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UIWearFragment extends Fragment {
+
+
+    public UIWearFragment() {
+        // Required empty public constructor
+    }
+
+    public static UIWearFragment create(int layoutID) {
+        Bundle args = new Bundle();
+        args.putInt("layoutID", layoutID);
+        UIWearFragment fragment = new UIWearFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        if (args != null) {
+            int layoutID = args.getInt("layoutID", 0);
+            if (layoutID != 0) {
+                return inflater.inflate(layoutID, container, false);
+            }
+        }
+        return inflater.inflate(R.layout.activity_container, container, false);
+    }
+
+}
